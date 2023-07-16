f3896ab0631a11692cf2824ca98077818ff08e07
Steven <mkim9002@gmail.com>
2023-07-16 18:21:22 +0900

    aster

---
+++ chat_gpt/basicGpt/app.py
@@ -0,0 +1,35 @@
+import json
+import os
+from flask import Flask, render_template, request, jsonify
+import openai
+from openai.error import RateLimitError
+
+app = Flask(__name__)
+
+# CHANGE OPENAI_API_KEY -> YOUR_API_KEY
+# EXAMPLE 
+# openai.api_key = "sk-JUg...."
+openai.api_key = "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"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gpt4', methods=['GET', 'POST'])
+def gpt4():
+    user_input = request.args.get('user_input') if request.method == 'GET' else request.form['user_input']
+    messages = [{"role": "user", "content": user_input}]
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        content = response.choices[0].message["content"]
+    except RateLimitError:
+        content = "The server is experiencing a high volume of requests. Please try again later."
+
+    return jsonify(content=content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
